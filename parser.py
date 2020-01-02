cdfa7a8a8c4b13d338f5eb328f337cdcd8ce5d86
Andrew Everitt <andreweveritt@e3d-online.com>
2020-01-02 13:44:13 +0000

    updated simplify3d gcode parser

--- parser.py
+++ parser.py
@@ -3,15 +3,23 @@ from math import ceil
 import re
 
 
-class GcodeLayer:
-    def __init__(self, name, gcode):
-        self.name = name
+class Simplify3DGcodeLayer:
+    def __init__(self, gcode, name=None, layer_height=None):
         self.gcode = gcode
+        self.name = name
+        self.layer_height = layer_height
 
-        self.layer_height = self.get_initial_height(self.gcode)
+        if name is None:
+            self.name = self.get_name(self.gcode)
+
+        if layer_height is None:
+            self.layer_height = self.get_initial_height(self.gcode)
+
+    def get_name(self, gcode):
+        return gcode.split(',')[0][2:]
 
     def get_initial_height(self, gcode):
-        return None
+        return gcode.split('\n')[0][15:]
 
 
 class Parser:
@@ -25,23 +33,27 @@ class Parser:
     def split_additive_layers(self, gcode_add):
         """ Takes Simplify3D gcode and splits in by layer """
         tmp_list = re.split('(; layer)', gcode_add)
-        gcode_add_layers = [{
-            "name": "initialise",
-            "gcode": tmp_list[0],
-            "layer_height": 0,
-        }]  # slicer settings & initialise
+
+        gcode_add_layers = []
+        gcode_add_layers.append(Simplify3DGcodeLayer(
+            tmp_list[0],
+            name="initialise",
+            layer_height=0,
+        ))    # slicer settings & initialise
 
         for i in range(ceil(len(tmp_list)/2)):
             if i == 0:
                 continue
 
             layer = tmp_list[2*i-1] + tmp_list[2*i]
+            name = layer.split(',')[0][2:]
 
-            gcode_add_layers.append({
-                "name": layer.split(',')[0][2:],
-                "gcode": layer,
-                "layer_height": layer.split('\n')[0][15:],
-            })
+            if 2*i == len(tmp_list) - 1:
+                gcode_add_layers.append(Simplify3DGcodeLayer(
+                    layer, 'end', gcode_add_layers[-1].layer_height))
+                continue
+
+            gcode_add_layers.append(Simplify3DGcodeLayer(layer))
 
         self.gcode_add_layers = gcode_add_layers
 
@@ -59,10 +71,10 @@ class Parser:
 
 
 if __name__ == "__main__":
-    gcode_add_file = open("additive_box.gcode", "r")
+    gcode_add_file = open("gcode/additive_box.gcode", "r")
     gcode_add = gcode_add_file.read()
 
-    gcode_sub_file = open("double_box_side_top_no_arc.nc", "r")
+    gcode_sub_file = open("gcode/double_box_side_top_no_arc.nc", "r")
     gcode_sub = gcode_sub_file.read()
 
     parser = Parser(gcode_add, gcode_sub)
