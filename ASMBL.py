ec0f2a447e8b931ac5217589dea40ae334ad5d2d
Andy Everitt <38423143+AndyEveritt@users.noreply.github.com>
2020-03-18 19:34:32 +0000

    remove excess ui setup code

--- ASMBL.py
+++ ASMBL.py
@@ -1,93 +1,95 @@
-import adsk.core, adsk.fusion, traceback
+import adsk.core
+import adsk.fusion
+import traceback
+
+# Global list to keep all event handlers in scope.
+# This is only needed with Python.
+handlers = []
+
 
 def run(context):
     ui = None
     try:
         app = adsk.core.Application.get()
-        ui  = app.userInterface
+        ui = app.userInterface
 
         # For this example, we are adding the already exisiting 'Extrude' command into a new panel:
         cmdDefinitions = ui.commandDefinitions
-        anotherExtrudeCmd = cmdDefinitions.itemById('Extrude')
-                
-        # For a few months, the customer might run either classic UI or tabbed toolbar UI.
-        # Find out what is being used:
-        runningTabbedToolbar = ui.isTabbedToolbarUI
 
-        if runningTabbedToolbar:
-            # Get all workspaces:
-            allWorkspaces = ui.workspaces
+        # Get all workspaces:
+        allWorkspaces = ui.workspaces
 
-            # Get the CAM workspace:
-            camWorkspace = allWorkspaces.itemById('CAMEnvironment')
-            if (camWorkspace):
-                # Get all the tabs for the CAM workspace:
-                allCamTabs = camWorkspace.toolbarTabs
-                if (allCamTabs.count > 0):
-                    # Add a new tab to the CAM workspace:
-                    newCamTab = allCamTabs.add('NewCamTabHere', 'ASMBL')
-                    if (newCamTab):
-                        # Get all of the toolbar panels for the NewCam tab:
-                        allNewCamTabPanels = newCamTab.toolbarPanels
+        # Get the CAM workspace:
+        camWorkspace = allWorkspaces.itemById('CAMEnvironment')
 
-                        # Has the panel been added already?
-                        # You'll get an error if you try to add this more than once to the tab.
+        # Get all the tabs for the CAM workspace:
+        allCamTabs = camWorkspace.toolbarTabs
 
-                        #Activate the Render Workspace before activating the newly added Tab
-                        camWorkspace.activate()
-                            
-                        brandNewCamPanel = None
-                        brandNewCamPanel = allNewCamTabPanels.itemById('bestCamPanelEverId')
-                        if brandNewCamPanel is None:
-                            # We have not added the panel already.  Go ahead and add it.
-                            brandNewCamPanel = allNewCamTabPanels.add('bestCamPanelEverId', 'Best Cam Panel')
+        # Add a new tab to the CAM workspace:
+        AsmblTab = allCamTabs.add('AsmblTab', 'ASMBL')
 
-                        if brandNewCamPanel:
-                            # We want this panel to be visible:
-                            brandNewCamPanel.isVisible = True
-                            # Access the controls that belong to the panel:
-                            newPanelControls = brandNewCamPanel.controls
+        # Get all of the toolbar panels for the NewCam tab:
+        allAsmblTabPanels = AsmblTab.toolbarPanels
 
-                            # Do we already have this command in the controls?  
-                            # You'll get an error if you try to add it more than once to the panel:
-                            extrudeCmdControl =  None
-                            extrudeCmdControl = newPanelControls.itemById('Extrude')
-                            if extrudeCmdControl is None:
-                            
-                            # Activate the newly added Tab in Render Workspace before adding commad to the Panel
-                                if camWorkspace.isActive: 
-                                    camTab = allCamTabs.itemById('NewCamTabHere')
-                                    if not camTab.isActive :
-                                        activationState = camTab.activate()
-                                        if activationState :
-                                            if anotherExtrudeCmd:
-                                                # Go ahead and add the command to the panel:
-                                                extrudeCmdControl = newPanelControls.addCommand(anotherExtrudeCmd)
-                                                if extrudeCmdControl:
-                                                    extrudeCmdControl.isVisible = True
-                                                    extrudeCmdControl.isPromoted = True
-                                                    extrudeCmdControl.isPromotedByDefault = True
-                                                    ui.messageBox('Do you see Best Cam Panel now?')
-                                            
-                            else:
-                                # If the command is already added to the Panel check if it is visible and display a message
-                                if camWorkspace.isActive: 
-                                    camTab = allCamTabs.itemById('NewCamTabHere')
-                                    if not camTab.isActive :
-                                        activationState = camTab.activate()
-                                        if activationState :
-                                            if brandNewCamPanel.isVisible:
-                                                ui.messageBox('Do you see Best Cam Panel now?')     
-                                            else:
-                                                totalControlsInPanel = newPanelControls.count
-                                                if (totalControlsInPanel == 1):
-                                                    if extrudeCmdControl.isVisible:
-                                                        ui.messageBox('Not visible control')
+        # Activate the Cam Workspace before activating the newly added Tab
+        camWorkspace.activate()
+
+        asmblSetupPanel = None
+        asmblSetupPanel = allAsmblTabPanels.itemById('asmblSetupPanelId')
+        if asmblSetupPanel is None:
+            # Add setup panel
+            asmblSetupPanel = allAsmblTabPanels.add('asmblSetupPanelId', 'ASMBL Setup Panel')
+
+        if asmblSetupPanel:
+            # We want this panel to be visible:
+            asmblSetupPanel.isVisible = True
+
+            # Create a button command definition.
+            setupButton = cmdDefinitions.addButtonDefinition(
+                'SetupButtonID', 'Setup Button', 'Create a new ASMBL setup')
+
+            # Connect to the command created event.
+            setupCreated = SetupCreatedEventHandler()
+            setupButton.commandCreated.add(setupCreated)
+            handlers.append(setupCreated)
+
+            # Add setup button to ASMBL setup panel
+            setupControl = asmblSetupPanel.controls.addCommand(setupButton)
 
     except:
         if ui:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
-			
+
+
+# Event handler for the commandCreated event.
+class SetupCreatedEventHandler(adsk.core.CommandCreatedEventHandler):
+    def __init__(self):
+        super().__init__()
+
+    def notify(self, args):
+        eventArgs = adsk.core.CommandCreatedEventArgs.cast(args)
+        cmd = eventArgs.command
+
+        # Connect to the execute event.
+        onExecute = SetupExecuteHandler()
+        cmd.execute.add(onExecute)
+        handlers.append(onExecute)
+
+
+# Event handler for the execute event.
+class SetupExecuteHandler(adsk.core.CommandEventHandler):
+    def __init__(self):
+        super().__init__()
+
+    def notify(self, args):
+        eventArgs = adsk.core.CommandEventArgs.cast(args)
+
+        # Code to react to the event.
+        app = adsk.core.Application.get()
+        ui = app.userInterface
+        ui.messageBox('In command execute event handler.')
+
+
 # When the addin stops we need to clean up the ui
 def stop(context):
     app = adsk.core.Application.get()
@@ -98,27 +100,27 @@ def stop(context):
         allToolbarPanels = ui.allToolbarPanels
 
         # See if our cam panel still exists
-        brandNewCamPanel = allToolbarPanels.itemById('bestCamPanelEverId')
-        if brandNewCamPanel is not None:
+        asmblSetupPanel = allToolbarPanels.itemById('asmblSetupPanelId')
+        if asmblSetupPanel is not None:
 
             # Remove the controls we added to our panel
-            for control in brandNewCamPanel.controls:
+            for control in asmblSetupPanel.controls:
                 if control.isValid:
                     control.deleteMe()
 
             # Remove our panel
-            brandNewCamPanel.deleteMe()
+            asmblSetupPanel.deleteMe()
 
         # Get all of the toolbar tabs
         allToolbarTabs = ui.allToolbarTabs
 
         # See if our render tab still exists
-        newCamTab = allToolbarTabs.itemById('NewCamTabHere')
-        if brandNewCamPanel is not None:
+        AsmblTab = allToolbarTabs.itemById('AsmblTab')
+        if asmblSetupPanel is not None:
 
             # Remove our render tab from the UI
-            if newCamTab.isValid:
-                newCamTab.deleteMe()
+            if AsmblTab.isValid:
+                AsmblTab.deleteMe()
 
     except:
         if ui:
