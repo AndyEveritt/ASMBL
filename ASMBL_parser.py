63f7e28883804244ca2783c194e4e677e4bbfe3f
AndyEveritt <andreweveritt@e3d-online.com>
2020-04-07 09:47:09 +0100

    [FIX] issue when layer overlap = 0

--- ASMBL_parser.py
+++ ASMBL_parser.py
@@ -421,13 +421,13 @@ class Parser:
                 later_additive = [layer for layer in self.gcode_add_layers[:-1]
                                   if layer.layer_height > op_instance.height]
 
-                if len(later_additive) >= layer_overlap:
-                    op_instance.layer_height = later_additive[layer_overlap - 1].layer_height
-
-                elif len(later_additive) == 0:   # no further printing
+                if len(later_additive) == 0:   # no further printing
                     # add 10 since it is unlikely that the printed layer height will exceed 10 mm
                     # but still want to place cutting after a print at the same height
                     op_instance.layer_height = op_instance.height + 10
+
+                elif len(later_additive) >= layer_overlap:
+                    op_instance.layer_height = later_additive[layer_overlap - 1].layer_height
 
                 else:
                     op_instance.layer_height = later_additive[-1].layer_height
